45b445e8e3ab92ec9b8afb0d27341ec4a5ff8bfb
Alexis La Goutte <alexis.lagoutte@gmail.com>
2025-04-15 15:21:59 +0200

    afc_lag_interfaces: fix global_config.native_vlan type (list -> int)

--- plugins/modules/afc_lag_interfaces.py
+++ plugins/modules/afc_lag_interfaces.py
@@ -80,7 +80,7 @@ options:
                         required: true
                     native_vlan:
                         description: Native VLAN
-                        type: list
+                        type: int
                         required: true
                     tagged:
                         description: tagged Native VLAN
